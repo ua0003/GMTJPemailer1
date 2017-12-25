bbb3505ffd9432c13c6061f7bcdf9667a71fe806
Joshua Machnik <ua000@gmail.com>
2017-12-24 21:38:31 -0500

    Fix: Apply getopt correctly for arg parsing.

---
+++ GMTJPemailer0.1.cpp
@@ -0,0 +1,67 @@
+//A command line program used for sending emails.
+
+#include <iostream>
+#include <regex>
+#include <string>
+#include <getopt.h>
+#include <cstdlib>
+
+using namespace std;
+const char* program_name;
+char* to, cc, bcc, subject,message;
+
+void print_usage(ostream& os, int exit_code) 
+	{
+		cout << "Usage : %s options [ inputfile ... ]\n" << program_name << endl;
+		cout << " -h --help Display this usage information." << endl
+		<< " -o --output filename Write output to file." << endl
+		<< " -v --verbose Print verbose messages." << endl;
+		exit (exit_code);
+	}
+
+int main(int argc, char*argv[])
+{
+	int optionCount;
+	const char* const short_options= "hvt:";
+
+	const struct option long_options[] = 
+	{
+		{ "help", 0, NULL, 'h' },
+		{ "version", 0, NULL, 'v' },
+		{ "to", 1, NULL, 't' },
+		{ NULL, 0, NULL, 0 }
+	}; 
+program_name = argv[0];
+
+	do 
+	{
+		optionCount = getopt_long (argc,argv,short_options,long_options,NULL);
+
+		switch (optionCount) 
+		{
+			case 'h':
+			print_usage(cout, 0);
+
+			case 'v':
+			cout<< "This is version 0.2 of GMTJPemailer.";
+			break;
+			
+			
+			case 't':
+			to = optarg;
+			break;
+
+			case '?':
+			print_usage (cerr, 1);
+
+			case -1:
+			break;
+
+			default:
+			abort();
+		}
+	} 		
+	while (optionCount != -1);
+	cout<<to;
+	return 0;
+}
