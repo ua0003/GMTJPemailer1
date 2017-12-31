70ee76c7af988e80a32066b24b71739e61d02968
Joshua Machnik <ua000@gmail.com>
2017-12-30 23:25:35 -0500

    Fix: Remove dummy regex and update regex to posix syntax.

--- GMTJPemailer.cpp
+++ GMTJPemailer.cpp
@@ -3,18 +3,68 @@
 #include <iostream>
 #include <regex>
 #include <string>
-#include "cxxopts.hpp"
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 
+static int verbose_flag; // --maybe don't need this? */
+//set name space
+using namespace std;	
 
-cxxopts::Options options("GMTJPemailer", "A command line emailer.");
-options.add_options()
-	("v,version", "Version", cxxopts::value<std::string>()->default_value(True))
-	("h,help", "Help", cxxopts::value<std::string>());
-	
 int main(int argc, char *argv[])
 {
-	using namespace std;
+	//ArgCount
+	int c;
+	while(1)
+	{
+		static struct option long_options[] =
+		{
+			//flagged options
+			//name, requires argument (0 = no, 1 = yes, 2 = optional),flag, value
+			
+			{"verbose", 0, &verbose_flag, 1},
+			{"brief", 0, &verbose_flag, 0},
+			//other options
+			{"help", 0, 0, 'h'},
+			{"version", 0, 0, 'v'},
+			{"to", 1, 0, 't'},
+//TODO: add other options:
+//cc - c
+//bcc - b
+//subject - s
+//message - m
+//attachment -a
+//configure - c
+			{0,0,0,0}
+		};
+	int option_index = 0;
+	
+	c = getopt_long(argc,argv,"hvt:", long_options, &option_index);
+	
+	//Detect the end of options.
+	if(c== -1)
+		break;
+		
+	switch(c)
+		{
+		case 0:
+        /* If this option set a flag, do nothing else now. */
+        if (long_options[option_index].flag != 0)
+            break;
+        cout << ("option %s", long_options[option_index].name);
+        if (optarg)
+            cout << (" with arg %s", optarg)<<endl;
+            break;
+          
+        case "h":
+			break;
+		case "v":
+			break;
+		case "t":
+			break;
+		}
+	}
 	
 	cout << "Hello World" <<endl;
 	cout << "This is my second line."<<endl;
