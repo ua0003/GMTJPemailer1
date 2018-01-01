67a69003f9e228a125b2fc0bb1fe89a5b437bcd4
Joshua Machnik <ua000@gmail.com>
2018-01-01 13:05:59 -0500

    Feat: Add include statement for curl.

--- GMTJPemailer1.cpp
+++ GMTJPemailer1.cpp
@@ -4,9 +4,10 @@
 //include statements
 #include <iostream>
 // need to add gmime still
-#include <gmime-3.0/gmime/gmime.h>
+#include <gmime/gmime.h>
 #include <glib-2.0/glib.h>
-#include "CSmtp.h"
+#include <curl/curl.h>
+//#include "CSmtp.h"
 #include <string>
 #include <cstring>
 #include <getopt.h>  //used for arg parsing options
