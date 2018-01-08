1aa3a8f935630b1933a0db9d2ac7ad41840b86a2
Joshua Machnik <ua000@gmail.com>
2018-01-07 21:48:24 -0500

    Fix: Make strings to /0 ending chars for libcurl.

--- GMTJPemailer1.cpp
+++ GMTJPemailer1.cpp
@@ -5,7 +5,7 @@
 #include <gmime/gmime.h>
 #include <glib-2.0/glib.h>
 #include <curl/curl.h>
-#include <string.h>
+
 #include <string>
 #include <cstring>
 #include <getopt.h>  //used for arg parsing options
@@ -62,7 +62,8 @@ void print_usage(ostream& os, int exit_code)
 //CONFIG FUNC
 //function prototype
 smtpConfig configSMTP();
-
+int mainCurl(void);
+//string smtpAddress(smtpConfig);
 
 int main(int argc, char* argv[])
 {
@@ -141,7 +142,7 @@ int main(int argc, char* argv[])
 	}
 	while (optionCount != -1);
 
-
+    mainCurl();
 	//cout statments for trouble shooting.
 //	cout<<to<<endl;
 //	cout<<cc<<endl;
@@ -237,7 +238,7 @@ smtpConfig configSMTP()
 	cin>>mysmtp.from_name;
     cout<<endl;
 
-	cout<<"Pleae enter the password youuse to access your email account."<<endl;
+	cout<<"Please enter the password that you use to access your email account."<<endl;
 	cin>>mysmtp.password;
 
 	//test out statements
@@ -251,12 +252,26 @@ smtpConfig configSMTP()
 }
 //THIS PAYLOAD NEEDS TO BE REPLACED BY GMIME header and parts...
 //static const char *payload_text[] = {
-list<string> payload_text {
-  "Date: Mon, 29 Nov 2010 21:54:29 +1100\r\n",
-  "To: " + to + "\r\n"//+ "\r\n",
-  "From: " + smtpObj.from_email_address +" (Example User)\r\n",
-  "Cc: " + cc + " (Another example User)\r\n",
-  "Bcc:" + bcc + "blind copied user.\r\n",
+//list<string> payload_text {
+//  "Date: Sun, 7 Jan 2018 21:54:29 +1100\r\n",
+//  "To: " + to + "\r\n"//+ "\r\n",
+//  "From: " + smtpObj.from_email_address +" (Example User)\r\n",
+//  "Cc: " + cc + " (Another example User)\r\n",
+//  "Bcc:" + bcc + "blind copied user.\r\n",
+//  "Message-ID: <dcd7cb36-11db-487a-9f3a-e652a9458efd@"
+//  "rfcpedant.example.org>\r\n",
+//  subject,
+//  "\r\n", /* empty line to divide headers from body, see RFC5322 */
+//  "The body of the message starts here.\r\n",
+//  "\r\n",
+//  "It could be a lot of lines, could be MIME encoded, whatever.\r\n",
+//  "Check RFC5322.\r\n",
+//  "\0"
+//};
+list<string> payload_text{
+"Date: Sun, 7 Jan 2018 21:54:29 +1100\r\n",
+  "To: ua0003@gmail.com \r\n"//+ "\r\n",
+  "From: " + string(smtpObj.from_email_address) +" (Example User)\r\n",
   "Message-ID: <dcd7cb36-11db-487a-9f3a-e652a9458efd@"
   "rfcpedant.example.org>\r\n",
   subject,
@@ -267,7 +282,6 @@ list<string> payload_text {
   "Check RFC5322.\r\n",
   "\0"
 };
-
 struct upload_status {
   int lines_read;
 };
@@ -280,6 +294,7 @@ static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp)
   if((size == 0) || (nmemb == 0) || ((size*nmemb) < 1)) {
     return 0;
   }
+//TODO need to determine len of payload...
 
 //  data = payload_text[upload_ctx->lines_read];
 //    vector<const char *> payloadText
@@ -312,35 +327,38 @@ int mainCurl(void)
 
   curl = curl_easy_init();
   if(curl) {
+    string preformatted = "smtp://";
+    string smtpName = string (smtpObj.name);
+    string smtpPort = smtpObj.port;
+    string formatted =  preformatted+smtpName+":"+smtpPort;
+    string usrNam = smtpObj.user_name;
+    string passWord = smtpObj.password;
+    string fromEmail = smtpObj.from_email_address;
+
+
     /* Set username and password */
-    curl_easy_setopt(curl, CURLOPT_USERNAME, smtpObj.user_name);
-    curl_easy_setopt(curl, CURLOPT_PASSWORD, smtpObj.password);
+//    curl_easy_setopt(curl, CURLOPT_USERNAME, smtpObj.user_name);
+//    curl_easy_setopt(curl, CURLOPT_PASSWORD, smtpObj.password);
+    curl_easy_setopt(curl, CURLOPT_USERNAME, usrNam.c_str());
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, passWord.c_str());
 
     /* This is the URL for your mailserver. Note the use of port 587 here,
      * instead of the normal SMTP port (25). Port 587 is commonly used for
      * secure mail submission (see RFC4403), but you should use whatever
      * matches your server configuration. */
-    curl_easy_setopt(curl, CURLOPT_URL, smtpObj.name&&":"&&smtpObj.port);
-
-    /* In this example, we'll start with a plain text connection, and upgrade
+     //smtp://user:password;options@mail.example.com
+//     cout<<*formatSMTP<<endl;
+//    curl_easy_setopt(curl, CURLOPT_URL,*formatSMTP);
+    curl_easy_setopt(curl,CURLOPT_URL,formatted.c_str());
+//      curl_easy_setopt(curl, CURLOPT_URL,"smtp://smtp.gmail.com:465");
+//      curl_easy_setopt(curl, CURLOPT_URL,"smtp://smtp.gmail.com:587");
+    /* In this example, we'll start with a plin text connection, and upgrade
      * to Transport Layer Security (TLS) using the STARTTLS command. Be careful
      * of using CURLUSESSL_TRY here, because if TLS upgrade fails, the transfer
      * will continue anyway - see the security discussion in the libcurl
      * tutorial for more details. */
     curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
 
-    /* If your server doesn't have a valid certificate, then you can disable
-     * part of the Transport Layer Security protection by setting the
-     * CURLOPT_SSL_VERIFYPEER and CURLOPT_SSL_VERIFYHOST options to 0 (false).
-     *   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-     *   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-     * That is, in general, a bad idea. It is still better than sending your
-     * authentication details in plain text though.  Instead, you should get
-     * the issuer certificate (or the host certificate if the certificate is
-     * self-signed) and add it to the set of certificates that are known to
-     * libcurl using CURLOPT_CAINFO and/or CURLOPT_CAPATH. See docs/SSLCERTS
-     * for more information. */
-    curl_easy_setopt(curl, CURLOPT_CAINFO, "/path/to/certificate.pem");
 
     /* Note that this option isn't strictly required, omitting it will result
      * in libcurl sending the MAIL FROM command with empty sender data. All
@@ -349,21 +367,23 @@ int mainCurl(void)
      * they could cause an endless loop. See RFC 5321 Section 4.5.5 for more
      * details.
      */
-    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, smtpObj.from_email_address);
+    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, fromEmail.c_str());
 
     /* Add two recipients, in this particular case they correspond to the
      * To: and Cc: addressees in the header, but they could be any kind of
      * recipient. */
        if(!to.empty())
-    {
-
-        const char * TO = to.c_str();
-        string allcc = cc+bcc;
-        const char * CC = allcc.c_str();
-
-        recipients = curl_slist_append(recipients, TO);
-        recipients = curl_slist_append(recipients, CC);
-    }
+        {
+            recipients = curl_slist_append(recipients, to.c_str());
+        }
+        if(!cc.empty())
+        {
+            recipients = curl_slist_append(recipients, cc.c_str());
+        }
+        if(!bcc.empty())
+        {
+            recipients = curl_slist_append(recipients, bcc.c_str());
+        }
     curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
 
     /* We're using a callback function to specify the payload (the headers and
